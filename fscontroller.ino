eb955d0375380d43fc04cf3bb40241fd2eb649eb
Fabio Bastian <irai852@gmail.com>
2022-05-05 21:18:12 +1000

    working pot control

--- fscontroller.ino
+++ fscontroller.ino
@@ -45,18 +45,19 @@ struct keys {
 
 keys masterAlternatorKeys = { 2, { KEY_LEFT_ALT, 'a' } };
 keys masterBatteryKeys = { 2, { KEY_LEFT_ALT, 'b' } };
-keys masterKeys = { 2, { KEY_LEFT_SHIFT, 'm' } };
+keys masterKeys = { 1, { 'M' } };
 keys avionics = { 1, { KEY_PAGE_UP } };
 keys atcKeys = { 2, { KEY_SCROLL_LOCK, KEY_HOME } };
 keys gearKeys = { 1, { 'g' } };
 keys carbHeatKeys = { 1, { 'h' } };
-keys pitotHeatKeys = { 2, { KEY_LEFT_SHIFT, 'h' } };
-keys mixtureIncreaseKeys = {3, { KEY_LEFT_SHIFT, KEY_LEFT_CTRL, KEY_F3 }};
-keys mixtureDecreaseKeys = {3, { KEY_LEFT_SHIFT, KEY_LEFT_CTRL, KEY_F2 }};
+keys pitotHeatKeys = { 1, { 'H' } };
+keys mixtureIncreaseKeys = { 3, { KEY_LEFT_SHIFT, KEY_LEFT_CTRL, KEY_F3 } };
+keys mixtureDecreaseKeys = { 3, { KEY_LEFT_SHIFT, KEY_LEFT_CTRL, KEY_F2 } };
 // keys mixtureIncreaseKeys = { 2, { KEY_LEFT_CTRL, 'k' } };
 // keys mixtureDecreaseKeys = { 2, { KEY_LEFT_ALT, 'k' } };
 keys mixtureLeanKeys = { 3, { KEY_LEFT_SHIFT, KEY_LEFT_CTRL, KEY_F1 } };
 keys mixtureRichKeys = { 3, { KEY_LEFT_SHIFT, KEY_LEFT_CTRL, KEY_F4 } };
+keys fuelPumpKeys = { 2, { KEY_LEFT_CTRL, 'D' } };
 keys taxiLightKeys = { 2, { KEY_LEFT_ALT, 'j' } };
 keys navLightKeys = { 2, { KEY_LEFT_ALT, 'n' } };
 keys strobeLightKeys = { 1, { 'o' } };
@@ -65,12 +66,12 @@ keys beaconLightKeys = { 2, { KEY_LEFT_ALT, 'h' } };
 keys viewDashboardFirstKeys = { 2, { KEY_LEFT_CTRL, '1' } };
 keys throtleIncreaseKeys = { 1, { KEY_F3 } };
 keys throtleDecreaseKeys = { 1, { KEY_F2 } };
-keys throtleCutKeys = { 1, {KEY_F1 } };
+keys throtleCutKeys = { 1, { KEY_F1 } };
 keys viewDashboardNextKeys = { 1, { 'a' } };
 // keys trimDown = { 2, { KEY_LEFT_CTRL, 't' } };
-// keys trimUp = { 2, { KEY_LEFT_SHIFT, 't' } };
-keys trimDown = {1, { KEYPAD_7 }};  // NOTE: This does not seem to work - Apr 22
-keys trimUp = {1, { KEYPAD_1 }}; // This key does not seem to work
+// keys trimUp = { 1, { 'T' } };
+keys trimDown = {1, { KEYPAD_7 }};  
+keys trimUp = {1, { KEYPAD_1 }}; 
 
 const uint8_t eventOn = 0;
 const uint8_t eventOff = 1;
@@ -80,7 +81,7 @@ const uint8_t eventPrev = 3;
 struct button {
   String name;
   uint8_t pin;
-  keys *keys[4];
+  keys* keys[4];
   bool state;
   unsigned long debounce;
   int count;
@@ -95,12 +96,12 @@ button buttons[nButtons] = {
   { "beacon light", 3, { &beaconLightKeys, &beaconLightKeys, NULL, NULL }, 0, 0, 0, 0 },
   { "taxi lights", 4, { &taxiLightKeys, &taxiLightKeys, NULL, NULL }, 0, 0, 0, 0 },
   { "pitot heat", 5, { &pitotHeatKeys, &pitotHeatKeys, NULL, NULL }, 0, 0, 0, 0 },
-  { "free", 6, { NULL, NULL, NULL, NULL }, 0, 0, 0, 0 },
+  { "fuel pump", 6, { &fuelPumpKeys, &fuelPumpKeys, NULL, NULL }, 0, 0, 0, 0 },
   { "free", 7, { NULL, NULL, NULL, NULL }, 0, 0, 0, 0 },
   { "master", 8, { &masterKeys, &masterKeys, NULL, NULL }, 0, 0, 0, 0 },
   { "avionics", 9, { &avionics, &avionics, NULL, NULL }, 0, 0 },
   { "gear", 10, { &gearKeys, &gearKeys, NULL, NULL }, 0, 0, 0, 0 },
-  { "carby heat", 11, { &carbHeatKeys, &carbHeatKeys, NULL, NULL }, 0, 0, 0, 0 },
+  { "carby heat", 11, { &carbHeatKeys, &carbHeatKeys, NULL, NULL }, 0, 0, 0, 0 }
 };
 
 const int nPressureButtons = 2;
@@ -111,21 +112,23 @@ button pressureButtons[nPressureButtons] = {
 
 // https://docs.arduino.cc/learn/electronics/potentiometer-basics
 const unsigned long potDebounceDelay = 64;  // the debounce time; increase if the output flickers
-const int nPotButtons = 3;
+const int nPotButtons = 2;
 button potButtons[nPotButtons] = {
-  { "a1", A1, { &throtleIncreaseKeys, &throtleCutKeys, &throtleIncreaseKeys, &throtleDecreaseKeys }, 0, 0, 0, 1024 },
+  // { "a1", A1, { &throtleIncreaseKeys, &throtleCutKeys, &throtleIncreaseKeys, &throtleDecreaseKeys }, 0, 0, 0, 1024 },
   { "a2", A2, { &mixtureRichKeys, &mixtureLeanKeys, &mixtureIncreaseKeys, &mixtureDecreaseKeys }, 0, 0, 0, 1024 },
   { "a3", A3, { NULL, NULL, NULL, NULL }, 0, 0, 0, 1024 }
 };
 
 // Rotary encoder for trim wheel
 // https://lastminuteengineers.com/rotary-encoder-arduino-tutorial/
-const uint8_t trimCLKPin = A4;
-const uint8_t trimDTPin = A5;
+const uint8_t trimCLKPin = A5;
+const uint8_t trimDTPin = A4;
 // const uint8_t trimSWPin =6;
 int trimCLKState;
 int trimCounter = 0;
 unsigned long trimDebounceTime = 0;  // the last time the output pin was toggled
+
+
 void pressKey(keys* keys, int repeat) {
   if (keys == NULL) {
     Serial.println("null keys");
@@ -137,10 +140,16 @@ void pressKey(keys* keys, int repeat) {
 
   for (i = 0; i < keys->len; i++) {
 #ifdef LIVE
-    Keyboard.press(keys->seq[i]);
+    int ret = Keyboard.press(keys->seq[i]);
+    if (ret != 1) {
+#ifndef LIVE
+      Serial.println("error in press");
+#endif
+      return;
+    }
 #endif
   }
-  repeat = repeatDelay * repeat;
+  // repeat = repeatDelay * repeat;
   if (repeat > 3000) {  // maximum 3000 ms delay
     repeat = 3000;
   }
@@ -148,15 +157,24 @@ void pressKey(keys* keys, int repeat) {
     repeat = repeatDelay;
   }
   delay(repeat);  // xbox does not work without this delay; need time for os to accept key press
+#ifndef LIVE
   if (repeat > repeatDelay) {
     Serial.print("delay=");
     Serial.println(repeat);
   }
+#endif
 
   for (i = keys->len; i > 0;) {
     i--;
 #ifdef LIVE
-    Keyboard.release(keys->seq[i]);
+    int ret = Keyboard.release(keys->seq[i]);
+    if (ret != 1) {
+#ifndef LIVE
+      Serial.println("error in release");
+#endif
+      Keyboard.releaseAll();  // attempt to release numpad keys - fix bug of non stop repeating key
+      return;
+    }
 #endif
   }
 
@@ -206,63 +224,77 @@ void processPot(button* b) {
   // return;
   // }
   int x = analogRead(b->pin);
-  // reading may oscilate between +1 and -1 volts; ignore
-  // https://forum.arduino.cc/t/debounce-a-potentiometer/75209
-  if (x >= b->count - 1 && x <= b->count + 1) {
+
+    if (x == 0 && b->count != 0) {
+    pressKey(b->keys[eventOff], 1);
+
+    Serial.print(b->name);
+    Serial.println("is off");
+    b->count = 0;
+   return;
+  }
+  if (x == 1023 && b->count != 1023) {
+    pressKey(b->keys[eventOn], 1);
+    Serial.print(b->name);
+    Serial.println("is at max");
+    b->count = 1023;
     return;
   }
+
+  // reading may oscilate between +1 and -1 volts; ignore
+  // https://forum.arduino.cc/t/debounce-a-potentiometer/7509
+  if (x >= b->count - 64 && x <= b->count + 64) {
+    return;
+  }
+#ifndef LIVE
   Serial.println(x);
+#endif
 
 
   int difference = x - b->count;
   b->count = x;
 
-  
+
+
+
   if (difference < 0) {
+
     Serial.print(b->name);
     Serial.print(" dec by");
     Serial.println(difference);
-    pressKey(b->keys[eventPrev], difference * -1 / 10 + 1);
-    return;
+// for (i=0; i> difference; i -= 10){
+    pressKey(b->keys[eventPrev], 1);
+// }
   } else {
+
     Serial.print(b->name);
     Serial.print(" inc by");
     Serial.println(difference);
-    pressKey(b->keys[eventNext], difference / 10 + 1);
+// for (i=0; i < difference; i+=10){
+    pressKey(b->keys[eventNext], 1);
+// }
   }
+// Keyboard.releaseAll(); // trying to fix repeating bug
 
-
-if (b->count ==0) {
-    pressKey(b->keys[eventOff],1);
-    Serial.print(b->name);
-    Serial.println("is off");
-    return;
-  }
-  if (b->count > b->max-2) {
-    pressKey(b->keys[eventOn],1);
-    Serial.print(b->name);
-    Serial.println("is at max");
-    return;
-  }
 
 }
 
-void toggle(button* b) {
+void processSwitch(button* b) {
   boolean x = digitalRead(b->pin);
   if (x == b->state) {
     return;
   }
 
   long t = millis();
-  if (t < b->debounce) {
-    return;
-  }
-  b->debounce = t + potDebounceDelay;
+  // if (t < b->debounce) {
+    // return;
+  // }
+  // b->debounce = t + potDebounceDelay;
   b->state = x;
   Serial.print(b->name);
   Serial.print("pin=");
   Serial.print(b->pin);
-    Serial.print(" keys=");
+  Serial.print(" keys=");
   Serial.print(b->keys[eventOn]->seq[0]);
   Serial.print("=");
   Serial.println(b->state);
@@ -279,12 +311,16 @@ void processPressureButton(button* b) {
     b->state = x;
     if (b->count == 0) {
       pressKey(b->keys[eventOn], 1);
+#ifndef LIVE
       Serial.print(b->name);
       Serial.println(" pressed");
+#endif
     } else {
       pressKey(b->keys[eventOff], 1);
+#ifndef LIVE
       Serial.print(b->name);
       Serial.println(" released");
+#endif
     };
     b->count++;
     if (b->count > b->max) {
@@ -318,6 +354,7 @@ void setup() {
   trimCLKState = digitalRead(trimCLKPin);  // Read the initial state of CLK
 
   Keyboard.begin();
+  Keyboard.releaseAll();  // release any key that is pressed
 }
 
 void loop() {
@@ -330,7 +367,7 @@ void loop() {
 
   int i;
   for (i = 0; i < nButtons; i++) {
-    toggle(&buttons[i]);
+    processSwitch(&buttons[i]);
   }
 
   for (i = 0; i < nPotButtons; i++) {
