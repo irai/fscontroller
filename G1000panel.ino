76db7ea91cf2e30ba815bb5d59505652c59187e4
Fabio Bastian <irai852@gmail.com>
2023-03-26 00:57:15 +1100

    G1000 support

--- G1000panel.ino
+++ G1000panel.ino
@@ -4,20 +4,20 @@
 button switchButtons[] = {};
 
 button pressureButtons[] = {
-  { "com 1/2 switch", 1, { &com1StbSwapKeys, &com1StbSwapKeys, &com1StbSwapKeys, &com1StbSwapKeys }, 0, 0, 0, 1 },
-  { "set heading bug", 11, { &setHeadingBugKeys, NULL, &setHeadingBugKeys, NULL }, 0, 0, 0, 1 },
-  { "set altitude", 16, { &setHeadingBugKeys, NULL, &setHeadingBugKeys, NULL }, 0, 0, 0, 1 },
-  { "swap com stdby", 26, { &com1StbSwapKeys, NULL, NULL, NULL }, 0, 0, 0, 1 }
+  { "com 1/2 switch", 1},
+  { "set heading bug", 11},
+  { "set altitude", 16},
+  { "swap com stdby", 26},
 };
 
 button potButtons[] = {};
 
 rotary rotaryControls[] = {
-  { "com freq", &focusFrequency, 27, &com1StbFreqUpKeys, 0, &com1StbFreqDownKeys, 1, &com1StbSwapKeys, 0, 0, 0 },
-  { "com freq dec", &focusFrequencyDec, 7, &com1StbFreqDecUpKeys, 8, &com1StbFreqDecDownKeys, 1, NULL, 0, 0, 0 },
-  { "heading bug", &focusReset, 9, &incHeadingBugKeys, 10, &decHeadingBugKeys, 11, &setHeadingBugKeys, 0, 0, 0 },
-  { "altitude", &focusReset, 12, &increaseAltKeys, 13, &decreaseAltKeys, 16, NULL, 0, 0, 0 },
-  { "altitude dec", &focusReset, 14, &increaseAltKeys, 15, &decreaseAltKeys, 16, NULL, 0, 0, 0 }
+  { "com freq", 27, 0, 1, 0,0,0,0,0  },
+  { "com freq dec", 7, 8, 1, 0,0, 0, 0, 0 },
+  { "heading bug", 9, 10, 11, 0,0,0, 0, 0 },
+  { "altitude", 12, 13, 16, 0,0, 0, 0, 0 },
+  { "altitude dec", 14, 15, 16, 0,0, 0, 0, 0 }
 };
 
 const int nSwitchButtons = sizeof(switchButtons)/sizeof(button);
