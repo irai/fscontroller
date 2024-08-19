451c04a246d4d78440a40fba5caba5dc015d1df0
Fabio Bastian <irai852@gmail.com>
2024-08-19 15:23:46 +1000

    kx165: add function to display frequency

--- kx165panel.ino
+++ kx165panel.ino
@@ -6,13 +6,13 @@
 /*
  Now we need a LedControl to work with.
  ***** These pin numbers will probably not work with your hardware *****
- pin 12 is connected to the DataIn 
- pin 11 is connected to the CLK 
- pin 10 is connected to LOAD 
+ pin 12 is connected to the DataIn
+ pin 11 is connected to the CLK
+ pin 10 is connected to LOAD
  ***** Please set the number of devices you have *****
  But the maximum default of 8 MAX72XX wil also work.
  */
-LedControl lc = LedControl(15, 14, 13, 2);
+LedControl lc = LedControl(15, 14, 13, 3);
 
 TM1637 lcd(16, 17);
 
@@ -37,23 +37,48 @@ const int nRotaryControls = sizeof(rotaryControls) / sizeof(rotary);
 const char* panelName = "kx165.com.nav.panel";
 const char* panelVersion = "1.0.0";
 
+const int com = 0;
+const int comStby = 1;
+const int nav = 2;
+const int navStby = 3;
+
+
+void displayFrequency(int com, const char* frequency) {
+  int len = strlen(frequency);
+  int displayIndex = 0;
+  bool dp = false;
+
+  // Display each character of the frequency string in reverse order
+  for (int i = len - 1; i >= 0; i--) {
+    if (frequency[i] == '.') {
+      dp = true; // Skip the decimal point, but set the decimal point on the next digit
+    } else {
+      lc.setChar(com, displayIndex, frequency[i], dp);
+      dp = false;
+      displayIndex++;
+    }
+  }
+}
+
+
 
 // initPanel is called at setup to initialise any panel specific variables
 int panelInit() {
   lcd.begin();
 
-  lc.shutdown(0, false);
-  lc.setIntensity(0, 4);
-  lc.setChar(0, 0, '8', true);
-  lc.setChar(0, 1, '7', false);
-  lc.setChar(0, 2, '6', false);
+  lc.shutdown(com, false);
+  lc.setIntensity(com, 4);
+  lc.shutdown(comStby, false);
+  lc.setIntensity(comStby, 4);
+  lc.shutdown(nav, false);
+  lc.setIntensity(nav, 4);
 
-  lc.shutdown(1, false);
-  lc.setIntensity(1, 4);
-  lc.setChar(1, 0, '5', false);
-  lc.setChar(1, 1, '4', false);
-  lc.setChar(1, 2, '3', false);
-  
+  displayFrequency(com, "132.80");
+
+  displayFrequency(comStby, "119.90");
+
+  displayFrequency(nav, "178.4");
+
   return 0;
 }
 
