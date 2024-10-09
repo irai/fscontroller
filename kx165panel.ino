ac664e60d636cd93fa0d9b47d85729005374a36d
Fabio Bastian <irai852@gmail.com>
2024-10-10 09:23:39 +1100

    kx165: turn on display when powered on

--- kx165panel.ino
+++ kx165panel.ino
@@ -15,9 +15,9 @@ led ledOutputs[] = {};
 
 
 button switchButtons[] = {
-  {.pin = DigitalPin(2,4), .action = "set_n", .function = &turnOnCom1, .variable = "index 1", .index = -1, .setValue = 0},
-  {.pin = DigitalPin(3,4), .action = "set_n", .function = &turnOnCom2, .variable = "index 2", .index = -1, .setValue = 0},
-  {.pin = DigitalPin(4,4), .action = "set_n", .function = &turnOnCom3, .variable = "index 3", .index = -1, .setValue = 0},
+  {.pin = DigitalPin(2,4), .action = "set_n", .function = &turnOnCom1, .variable = "index 1", .index = -1, .setValue = 0}, // radio 1  
+  {.pin = DigitalPin(3,4), .action = "set_n", .function = &turnOnCom2, .variable = "index 2", .index = -1, .setValue = 0}, // radio 2
+  {.pin = DigitalPin(4,4), .action = "set_n", .function = &turnOnCom3, .variable = "index 3", .index = -1, .setValue = 0}, // radio 3
   {.pin = DigitalPin(10,4), .action = "set_n", .function = &defaultSwitchFireOnLowFunction, .variable = "nav_radio_standby_swap", .index = -1, .setValue = 0},
   {.pin = DigitalPin(22,4), .action = "set_n", .function = &defaultSwitchFireOnLowFunction, .variable = "com_radio_standby_swap", .index = -1, .setValue = 0},
 };
@@ -25,10 +25,10 @@ button switchButtons[] = {
 pot potControls[] = {};
 
 rotary rotaryControls[] = {
-  {.aPin= DigitalPin(19, 1), .bPin= DigitalPin(18,1), .action = "inc_n", .function = &defaultRotaryFunction, .variable = "com_radio_freq_whole", .index = 0 },
-  {.aPin= DigitalPin(21, 1), .bPin= DigitalPin(20,1), .action = "inc_n", .function = &defaultRotaryFunction, .variable = "com_radio_freq_fract", .index = 0 },
-  {.aPin= DigitalPin(12, 2), .bPin= DigitalPin(11,2), .action = "inc_n", .function = &defaultRotaryFunction, .variable = "nav_radio_freq_whole", .index = 0 },
-  {.aPin= DigitalPin(9, 2), .bPin= DigitalPin(8,2), .action = "inc_n", .function = &defaultRotaryFunction, .variable = "nav_radio_freq_fract", .index = 0 },
+  {.aPin = DigitalPin(19, 2), .bPin = DigitalPin(18,2), .action = "inc_n", .function = &defaultRotaryFunction, .variable = "com_radio_freq_whole", .index = 0 },
+  {.aPin = DigitalPin(21, 2), .bPin = DigitalPin(20,2), .action = "inc_n", .function = &defaultRotaryFunction, .variable = "com_radio_freq_fract", .index = 0 },
+  {.aPin = DigitalPin(12, 2), .bPin = DigitalPin(11,2), .action = "inc_n", .function = &defaultRotaryFunction, .variable = "nav_radio_freq_whole", .index = 0 },
+  {.aPin = DigitalPin(9, 2), .bPin = DigitalPin(8,2), .action = "inc_n", .function = &defaultRotaryFunction, .variable = "nav_radio_freq_fract", .index = 0 },
 };
 
 const int nLedOutputs = sizeof(ledOutputs) / sizeof(led);
@@ -57,10 +57,10 @@ void turnOff() {
 }
 
 void turnOn() {
- if (kx165On) { // can be called multiple times when you switch between com1, com2, com3
+  if (kx165On) { // can be called multiple times when you switch between radio1, radio2, radio3
     return;
   }
-  kx165On = true; 
+  kx165On = true;
 
   comDisplay.shutdown(com, false);
   comDisplay.setIntensity(com, 1);
@@ -84,7 +84,7 @@ void turnOnCom1(SerialMsg* s, button* b) {
     return;
   }
   if (kx165Index == 1) {
-    updateIndex(0); 
+    updateIndex(0);
     turnOff();
   }
 }
@@ -107,13 +107,13 @@ void turnOnCom3(SerialMsg* s, button* b) {
 
 void updateIndex(int index) {
   for (int i = 0; i < nSwitchButtons; i++) {
-      switchButtons[i].index = index;
+    switchButtons[i].index = index;
   }
   for (int i = 0; i < nRotaryControls; i++) {
-      rotaryControls[i].index = index;
+    rotaryControls[i].index = index;
   }
   for (int i = 0; i < nPotButtons; i++) {
-      potControls[i].index = index;
+    potControls[i].index = index;
   }
   kx165Index = index;
 }
@@ -143,6 +143,16 @@ void displayFrequency(LedControl& display, int com, const char* frequency) {
 // initPanel is called at setup to initialise any panel specific variables
 int panelInit() {
   turnOff();
+  // TODO: fix this hack to turn on
+  if (switchButtons[0].pin.getPreviousState() == LOW) {
+    turnOnCom1(serialMsg, &switchButtons[0]);
+  }
+  if (switchButtons[1].pin.getPreviousState() == LOW) {
+    turnOnCom2(serialMsg, &switchButtons[1]);
+  }
+  if (switchButtons[2].pin.getPreviousState() == LOW) {
+    turnOnCom3(serialMsg, &switchButtons[2]);
+  }
   return 0;
 }
 
